63d7917624aefea57586483f986fa633ca693208
mojtaba eslami <mojtabaeslami.72@gmail.com>
2023-11-05 22:56:06 +0330

    fix lint-staged type checking

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '{apps,libs,tools}/**/*.{ts,tsx}': (files) => {
+  'apps/**/*.{ts,tsx}': (files) => {
     return `nx affected --target=typecheck --files=${files.join(',')}`;
   },
-  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': [
+  'apps/**/*.{js,ts,jsx,tsx,json}': [
     (files) => `nx affected:lint --files=${files.join(',')}`,
     (files) => `nx format:write --files=${files.join(',')}`,
   ],
