ea83ad6a3cfe27d949715466fc1f4e82178f8da4
mojtaba eslami <mojtabaeslami.72@gmail.com>
2023-11-18 19:05:35 +0330

    add google tag manager

--- apps/quick-quills/app/[locale]/layout.tsx
+++ apps/quick-quills/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from './Providers';
 import { notFound } from 'next/navigation';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -26,7 +27,24 @@ export default function LocalLayout({ children, params }: LocalLayoutProps) {
   if (!isValidLocale) notFound();
   return (
     <html lang={params.locale} className={inter.variable}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[-1],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WNHF3SF7');
+        `}
+      </Script>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WNHF4SF7"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Providers>{children}</Providers>
       </body>
     </html>
